953088ae41600568cac3e79bf04a365e24fc3bcb
adeliaayu <adeliaayut53@gmail.com>
2020-08-19 20:52:38 +0700

    add menu for selected mode to display data

--- app/src/main/java/com/dicoding/kotlin/myrecyclerview2/MainActivity.kt
+++ app/src/main/java/com/dicoding/kotlin/myrecyclerview2/MainActivity.kt
@@ -2,6 +2,8 @@ package com.dicoding.kotlin.myrecyclerview2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -41,5 +43,29 @@ class MainActivity : AppCompatActivity() {
         rv_heroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rv_heroes.adapter = listHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+                showRecyclerList()
+            }
+            R.id.action_grid -> {
+
+            }
+            R.id.action_cardview -> {
+
+            }
+        }
     }
 }
